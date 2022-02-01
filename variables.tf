14e53be3156403ad7caedd2701764b03ce58ac17
Alex Harness <mocofound@gmail.com>
2022-02-01 12:43:28 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,6 @@ variable "gcp_creds" {
   description = "GCP JSON Credentials"
 }
 
-variable "project_id" {
-  description = "Google Cloud Project ID"
-}
+#variable "project_id" {
+#  description = "Google Cloud Project ID"
+#}

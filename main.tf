adc6be0e7d9f7e4f99c660349ac24122287ece02
Alex Harness <mocofound@gmail.com>
2022-02-02 11:25:14 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,3 +28,26 @@ resource "vault_gcp_secret_roleset" "roleset" {
     ]
   }
 }
+
+resource "google_service_account" "this" {
+  account_id = "my-awesome-account"
+  display_name = "Service Account"
+}
+
+resource "vault_gcp_secret_static_account" "static_account" {
+  backend        = vault_gcp_secret_backend.gcp.path
+  static_account = "project_viewer"
+  secret_type    = "access_token"
+  token_scopes   = ["https://www.googleapis.com/auth/cloud-platform"]
+
+  service_account_email = google_service_account.this.email
+  
+  # Optional
+  binding {
+    resource = "//cloudresourcemanager.googleapis.com/projects/${google_service_account.this.project}"
+
+    roles = [
+      "roles/owner",
+    ]
+  }
+}
